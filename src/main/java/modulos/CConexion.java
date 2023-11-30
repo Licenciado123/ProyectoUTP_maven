aade343c51b7472eaf7aa1ad19926a73b7702d14
BuildTools <unconfigured@null.spigotmc.org>
2023-11-30 17:44:00 -0500

    99.0%

--- src/main/java/modulos/CConexion.java
+++ src/main/java/modulos/CConexion.java
@@ -19,7 +19,7 @@ public class CConexion {
         try {
             String cadena = "jdbc:sqlserver://localhost:"+puerto+";"+"databaseName="+bd+";integratedSecurity=false; trustServerCertificate=true";
             conectar = DriverManager.getConnection(cadena,usuario,contrasenia);
-            JOptionPane.showMessageDialog(null, "Se conecto correctamente a la base de datos");
+            //JOptionPane.showMessageDialog(null, "Se conecto correctamente a la base de datos");
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null,"Error al conectar a la base de datos, error: "+e.toString());
         }
